cee336ab0465b296153f4d89123add593124a214
Intheknowmos <97744853+Intheknowmos@users.noreply.github.com>
2026-01-26 14:00:30 +0100

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -27,6 +27,7 @@ const router = createBrowserRouter([
       { path: '/', element: <Home /> },
       { path: '/login', element: <Login /> },
       { path: '/signup', element: <SignUp /> },
+      { path: '/navbar', element: <NavBar /> },
       { path: '/about-us', element: <About /> },
       { path: '/cart', element: <Cart /> },
       { path: '/wishlist', element: <WishList /> },
