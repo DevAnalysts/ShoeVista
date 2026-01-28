4451e79e4adf3a2516399119c8fffcfc311bda86
YashSutariya02 <yashsutariya005@gmail.com>
2026-01-28 15:09:47 +0530

    Commit Test Task Code Changes

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -13,28 +13,7 @@ const About = () => {
                 designed to provide a seamless shopping experience with a diverse array
                 of options for men, women, and kids.
             </p>
-            <p className="text-gray-600 my-4">
-                At ShoeVista, we understand that comfort and style go hand in hand.
-                That's why our collection features everything from high-performance
-                running shoes to versatile walking sneakers. Whether you’re gearing up
-                for a marathon, enjoying a casual stroll, or simply seeking everyday
-                comfort, our range includes something for every occasion and preference.
-                Our assortment of men’s shoes combines rugged durability with modern
-                design, while our women’s collection offers both elegance and
-                practicality. For the little ones, we have a variety of fun and
-                functional options that ensure they step out in style.
-            </p>
-            <p className="text-gray-600 my-4">
-                Shopping with us means you have access to the latest trends and
-                innovations in footwear, all from the comfort of your home. Our
-                user-friendly online platform allows you to easily navigate through
-                categories, find detailed product information, and enjoy special
-                promotions. With our commitment to exceptional customer service and
-                fast, reliable delivery across India, you can trust us to deliver your
-                chosen pair right to your doorstep swiftly and securely.
-            </p>
         </div>
-
     );
 };
 
